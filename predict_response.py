f3c644bf5f37b8f251778162a6293e7e1a97eae9
shravanidunna <76036390+shravanidunna@users.noreply.github.com>
2023-05-29 19:20:52 +0530

    Add files via upload

---
+++ predict_response.py
@@ -0,0 +1,34 @@
+#Text Data Preprocessing Lib
+import nltk
+
+import json
+import pickle
+import numpy as np
+import random
+
+ignore_words = ['?', '!',',','.', "'s", "'m"]
+
+# Model Load Lib
+import tensorflow
+from data_preprocessing import get_stem_words
+
+model = tensorflow.keras.models.load_model('./chatbot_model.h5')
+
+# Load data files
+intents = json.loads(open('./intents.json').read())
+words = pickle.load(open('./words.pkl','rb'))
+classes = pickle.load(open('./classes.pkl','rb'))
+
+
+def preprocess_user_input(user_input):
+
+    input_word_token_1 = nltk.word_tokenize(user_input)
+    input_word_token_2 = get_stem_words(input_word_token_1, ignore_words) 
+    input_word_token_2 = sorted(list(set(input_word_token_2)))
+
+    bag=[]
+    bag_of_words = []
+   
+    # Input data encoding 
+    
+
